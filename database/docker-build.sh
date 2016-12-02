b0bd064ce8f03c3d0a2aee668ad74a3858be6551
rrowlands <rrowlands@terraframe.com>
2016-12-02 12:14:55 -0700

    Added SQL patch for allowing analytics on demo datasets

--- database/docker-build.sh
+++ database/docker-build.sh
@@ -16,6 +16,34 @@ function validate_parameters {
   fi
 }
 
+function patch_demo_db {
+  gunzip dhis2-db.sql.gz
+
+  # The demo database has the ability to run analytics disabled. This SQL enables the functionality.
+  echo "
+    INSERT INTO userrolemembers 
+    SELECT b.userroleid,a.userid from 
+    (SELECT userid from users where username = 'admin' ) a
+    CROSS JOIN (SELECT userroleid from userrole where name = 'System administrator (ALL)') b;
+  " >> dhis2-db.sql
+
+  gzip dhis2-db.sql
+}
+
+function fetch_data {
+  rm -rf dhis2-db.sql.gz
+
+  if [ $DATA_SET == "sierra-leone" ]; then
+    curl -L -o dhis2-db.sql.gz https://github.com/dhis2/dhis2-demo-db/blob/master/$DATA_SET/$VERSION_TMP/dhis2-db-$DATA_SET.sql.gz?raw=true
+  elif [ $DATA_SET == "trainingland" ]; then
+    curl -L -o dhis2-db.sql.gz https://github.com/dhis2/dhis2-demo-db/raw/master/trainingland/trainingland.sql.gz?raw=true
+  elif [ $DATA_SET == "world-adoption" ]; then
+    curl -L -o dhis2-db.sql.gz https://github.com/dhis2/dhis2-demo-db/raw/master/world-adoption/world-adoption.sql.gz?raw=true
+  fi
+  
+  patch_demo_db
+}
+
 while getopts "v:d:" OPTION
 do
   case $OPTION in
@@ -29,7 +57,6 @@ validate_parameters
 
 VERSION_TMP=${DHIS2_VERSION//[-._]/}
 
-#rm -rf dhis2-db.sql.gz
+fetch_data
 
-#curl -L -o dhis2-db.sql.gz https://github.com/dhis2/dhis2-demo-db/blob/master/$DATA_SET/$VERSION_TMP/dhis2-db-$DATA_SET.sql.gz?raw=true
 docker build -t pgracio/dhis2-db:$DHIS2_VERSION-$DATA_SET .
